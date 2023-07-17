8e99f4a7cc18ab7c42b2c978b4d1a02f095ce342
naimulabeer <naimul.h.abeer@gmail.com>
2023-07-18 01:47:01 +0600

    homepage created and some pic added

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,21 @@
-import { Link } from "react-router-dom";
-import PageNav from "../components/PageNav";
+import styles from "./Homepage.module.css";
 
 function Homepage() {
   return (
-    <div>
-      <PageNav />
-      <h1 className="test">My Homepage</h1>
-      <Link to="/app">Go to the App</Link>
-    </div>
+    <main className={styles.homepage}>
+      <section>
+        <h1>
+          You Travel The World.
+          <br />
+          Worldwise keeps track of your adventure.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+      </section>
+    </main>
   );
 }
 
