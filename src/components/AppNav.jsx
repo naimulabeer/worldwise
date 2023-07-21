d154a0cf2a22fddc7442969875dc13d4ce22d48f
naimulabeer <naimul.h.abeer@gmail.com>
2023-07-21 17:38:56 +0600

    nested routes and index route

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,7 +1,17 @@
+import { NavLink } from "react-router-dom";
 import styles from "./AppNav.module.css";
 
 function AppNav() {
-  return <nav className={styles.nav}>App Navigation</nav>;
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default AppNav;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import Logo from "./Logo";
 import AppNav from "./AppNav";
 import styles from "./Sidebar.module.css";
+import { Outlet } from "react-router-dom";
 
 function Sidebar() {
   return (
@@ -8,7 +9,7 @@ function Sidebar() {
       <Logo />
       <AppNav />
 
-      <p>List of cities</p>
+      <Outlet />
       <footer className={styles.footer}>
         &copy; Copyright {new Date().getFullYear()} by Worldwise inc.
       </footer>
